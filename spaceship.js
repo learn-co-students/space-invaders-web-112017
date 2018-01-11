bf6a1a16ac11333011673f0e3eb00adc2505fe4f
Matt McAlister <matt.mcalister93@gmail.com>
2018-01-11 13:55:46 -0500

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields){
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    if (this.crew.length === 0){
+      this.docked = true;
+    } else {
+      this.docked = false;
+    };
+    this.phasersCharge = "uncharged";
+    const ship = this
+    this.crew.forEach(function(crewMember){
+      crewMember.currentShip = ship;
+    });
+  }
+}
