d023f0132155190272cb20b93671ea6c61da1a62
mattfaircloth <mattcfaircloth@gmail.com>
2018-01-12 07:54:08 -0500

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,22 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    //this.id = ++spaceshipId
+    this.name = name;
+    this.phasers = phasers;
+    this.shields = shields
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.crew = crew;
+      if (this.crew.length === 0){
+        this.docked = true;
+      } else {
+        this.docked = false;
+      };
+
+      this.phasersCharge = "uncharged";
+      const ship = this
+      this.crew.forEach(function(crewMember){
+        crewMember.currentShip = ship;
+      });
+    }
+}
