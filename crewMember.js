2bb0d95dc4acbe29e952773f9abb1745ca1f4373
github username <github email address>
2018-01-11 20:37:10 -0500

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,30 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position){
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+  }
+
+  engageWarpDrive(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+
+
+  }
+  setsInvisibility(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+
+    if (this.position === 'Defender'){
+      this.currentShip.cloaked = true
+    } else {
+      return 'had no effect'
+    }
+  }
+  chargePhasers(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+  }
+}
